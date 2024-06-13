ffadd26ada4dca47d658115f03830aeb31b84f35
ootoro29 <gobou256@gmail.com>
2024-06-13 19:25:21 +0900

    connectを挟んだ

--- app/api/testAPI/route.tsx
+++ app/api/testAPI/route.tsx
@@ -7,8 +7,9 @@ type User = {
 
 export const GET = async(req:NextRequest) => {
   try {
-    const rows = await pool.query<User>('SELECT * FROM users'); 
-    return new NextResponse("Hello",{status:200});
+    const connect = await pool.connect();
+    const rows = await connect.query<User>('SELECT * FROM users'); 
+    return new NextResponse(JSON.stringify(rows.rows),{status:200});
   } catch (error) {
     throw error
   }
