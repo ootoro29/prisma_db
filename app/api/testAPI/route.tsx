48522ce68a1ce5acbec0fab7e1a28a874f841499
ootoro29 <gobou256@gmail.com>
2024-06-13 19:09:02 +0900

    poolのエラーを調べる

--- app/api/testAPI/route.tsx
+++ app/api/testAPI/route.tsx
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-//import pool from "@/db";
+import pool from "@/db";
+type User = {
+  id: number;
+  name: string;
+};
 
 export const GET = async(req:NextRequest) => {
   try {
-    //const { rows } = await pool.query('SELECT * FROM users');
+    //const { rows } = await pool.query<User>('SELECT * FROM users');
     return NextResponse.json({"message":"Hello"},{status:200});
   } catch (error) {
     throw error
