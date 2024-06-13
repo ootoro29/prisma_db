daeddb566e0828c68a64aeb157a4a3b3eab705e6
ootoro29 <gobou256@gmail.com>
2024-06-13 19:05:03 +0900

    poolをいったん削除

--- app/api/testAPI/route.tsx
+++ app/api/testAPI/route.tsx
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import pool from "@/db";
+//import pool from "@/db";
 
 export const GET = async(req:NextRequest) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM users');
-    return NextResponse.json(rows);
+    //const { rows } = await pool.query('SELECT * FROM users');
+    return NextResponse.json({"message":"Hello"},{status:200});
   } catch (error) {
     throw error
   }
