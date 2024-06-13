53671021835f55551c37434467cbda2a2eb4f29b
ootoro29 <gobou256@gmail.com>
2024-06-13 19:01:45 +0900

    User Typeを削除

--- app/api/testAPI/route.tsx
+++ app/api/testAPI/route.tsx
@@ -1,13 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import pool from "@/db";
-type User = {
-  id: number;
-  name: string;
-};
 
 export const GET = async(req:NextRequest) => {
   try {
-    const { rows } = await pool.query<User>('SELECT * FROM users');
+    const { rows } = await pool.query('SELECT * FROM users');
     return NextResponse.json(rows);
   } catch (error) {
     throw error
