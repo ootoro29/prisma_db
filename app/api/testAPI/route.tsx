a5b90161740004e5cb6bd59c183fdf50ca0df2a4
ootoro29 <gobou256@gmail.com>
2024-06-13 19:20:28 +0900

    rowsを無視

--- app/api/testAPI/route.tsx
+++ app/api/testAPI/route.tsx
@@ -8,7 +8,7 @@ type User = {
 export const GET = async(req:NextRequest) => {
   try {
     const rows = await pool.query<User>('SELECT * FROM users'); 
-    return new NextResponse(JSON.stringify(rows.rows),{status:200});
+    return new NextResponse("Hello",{status:200});
   } catch (error) {
     throw error
   }
