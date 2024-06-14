ec273c8a7601d1b8dba873f87cf45c175a09438d
ootoro29 <gobou256@gmail.com>
2024-06-14 15:47:16 +0900

    環境変数の書き方変更

--- db.ts
+++ db.ts
@@ -1,7 +1,18 @@
 import{Pool} from 'pg';
 import { Client } from 'pg';
 
+/*
 export const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
+*/
 
+export const client = new Client({
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || "5432"),
+  database: process.env.DB_NAME || 'postgres',
+  ssl: ((process.env.DB_SSL && process.env.DB_SSL.toLowerCase() == 'true') || false)
+    ? { rejectUnauthorized: false } : false,
+});
