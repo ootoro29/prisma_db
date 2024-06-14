397c69c96d8362d1715d24c36029296b74e191c2
ootoro29 <gobou256@gmail.com>
2024-06-14 14:00:59 +0900

    clientでクエリが出せるかどうか

--- app/api/test-api/route.tsx
+++ app/api/test-api/route.tsx
@@ -11,8 +11,9 @@ export const GET = async(req:NextRequest) => {
     const rows = await client.query<User>('SELECT * FROM users'); 
     //const connect = await pool.connect();
     //const rows = await pool.query<User>('SELECT * FROM users'); 
-    return new NextResponse("hello",{status:200});
+    return new NextResponse(JSON.stringify(rows),{status:200});
   } catch (error) {
     throw error
   }
 }
+ 
