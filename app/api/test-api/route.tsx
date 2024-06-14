f01bb29907b210001d58d1d2b4b77b625e5e8be4
ootoro29 <gobou256@gmail.com>
2024-06-14 14:24:56 +0900

    connect の中身追加

--- app/api/test-api/route.tsx
+++ app/api/test-api/route.tsx
@@ -7,10 +7,10 @@ type User = {
 
 export const GET = async(req:NextRequest) => {
   try {
-    await client.connect();
+    await client.connect(err => {
+      if(err) throw err;
+    });
     const rows = await client.query<User>('SELECT * FROM users'); 
-    //const connect = await pool.connect();
-    //const rows = await pool.query<User>('SELECT * FROM users'); 
     return new NextResponse(JSON.stringify(rows),{status:200});
   } catch (error) {
     throw error
